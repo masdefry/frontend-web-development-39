26bd87b02a2036f131fc6fffeaaa0d8e08e87804
M Defryan <muhammaddefryan@gmail.com>
2026-05-19 13:45:34 +0700

    day03: intro to tailwindcss

--- intro-react/src/pages/profile/index.tsx
+++ intro-react/src/pages/profile/index.tsx
@@ -1,10 +1,21 @@
-import Button from "../../components/Button";
+import Button from '../../components/Button';
 
-export default function ProfilePage(){
-    return(
-        <>
-            <h1>Profile Page</h1>
-            <Button title='Save Profile' />
-        </>
-    )
+export default function ProfilePage() {
+  return (
+    <>
+      <h1 className='mt-10 text-5xl text-red-800 font-bold'>
+        Everyone has the right
+      </h1>
+      <div className='grid grid-cols-5 gap-4 bg-red-100'>
+        <div className='bg-yellow-400 w-[150px] h-[150px]'>01</div>
+        <div className='bg-yellow-400 w-[150px] h-[150px]'>01</div>
+        <div className='bg-yellow-400 w-[150px] h-[150px]'>01</div>
+        <div className='bg-yellow-400 w-[150px] h-[150px]'>01</div>
+        <div className='bg-yellow-400 w-[150px] h-[150px]'>01</div>
+      </div>
+      <div className='flex justify-center items-center bg-gray-100'>
+        <h1 className='text-red-500'>Hello, World!</h1>
+      </div>
+    </>
+  );
 }
