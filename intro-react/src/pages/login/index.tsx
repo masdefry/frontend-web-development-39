aea7d04b6c134ce669669135caf3ebe19a09e1b4
M Defryan <muhammaddefryan@gmail.com>
2026-05-19 11:47:03 +0700

    day03: link to router dom

--- intro-react/src/pages/login/index.tsx
+++ intro-react/src/pages/login/index.tsx
@@ -1,10 +1,16 @@
-import Button from "../../components/Button";
+import { Link } from 'react-router-dom';
+import Button from '../../components/Button';
 
-export default function LoginPage(){
-    return(
-        <>
-            <h1>Login Page</h1>
-            <Button title='Submit Login' />
-        </>
-    )
+
+export default function LoginPage() {
+  return (
+    <>
+      <h1>Login Page</h1>
+      <Button title='Submit Login' />
+
+      <Link to='/profile-user'>Go to Profile Page</Link>
+
+      {/* <a href='/profile-user'>Go to Profile Page</a> */}
+    </>
+  );
 }
