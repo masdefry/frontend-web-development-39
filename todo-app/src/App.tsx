13cd0e1c3a33f492eac6da90e90e3813a5b72235
M Defryan <muhammaddefryan@gmail.com>
2026-05-21 14:30:49 +0700

    day05: responsive layout todo-app

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -5,11 +5,11 @@ export default function HomePage() {
     <>
       <div className='w-screen h-screen bg-gray-200'>
         <div className='relative'>
-          <div className="bg-[url('/images/background-banner.png')] h-50">
+          <div className="bg-[url('/images/background-banner.png')] bg-center h-50">
             <div className='bg-linear-to-br from-indigo-500/70 via-purple-500/70 to-pink-500/10 h-50'></div>
           </div>
           <div className='absolute top-10 w-screen flex justify-center'>
-            <div className='w-96'>
+            <div className='w-72 md:w-96'>
               <div className='flex justify-between items-center text-white'>
                 <h1 className='tracking-widest text-2xl'>TODO</h1>
                 <IoMoonSharp className='text-2xl' />
@@ -54,7 +54,7 @@ export default function HomePage() {
                 </li>
                 <li className='flex justify-between items-center text-xs text-gray-300 px-3 py-2'>
                   <h6>5 Items Left</h6>
-                  <div className='flex items-center gap-3'>
+                  <div className='hidden md:flex items-center gap-3'>
                     <h6>All</h6>
                     <h6>Active</h6>
                     <h6>Completed</h6>
@@ -62,6 +62,12 @@ export default function HomePage() {
                   <h6>Clear Completed</h6>
                 </li>
               </ul>
+
+              <div className='flex items-center gap-3 md:hidden bg-white mt-3 rounded-sm justify-center p-1 text-gray-300 text-xs'>
+                <h6>All</h6>
+                <h6>Active</h6>
+                <h6>Completed</h6>
+              </div>
             </div>
           </div>
         </div>
